eace51d98be95a9c20f213b7004276449bf38f28
Frozen <1884084+xrdavies@users.noreply.github.com>
2022-03-18 22:43:34 +0800

    add test

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import { Octokit, App } from "octokit";
+import { queue } from 'd3-queue';
 // import { Hat } from "hat";
 
 // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
@@ -40,11 +41,46 @@ const octokit = new Octokit({ auth: `${process.env.PERSONAL_TOKEN}` });
 {
     let {
         data
-    } = await octokit.rest.git.getTree({ owner: "xrdavies", repo: "github-db", tree_sha: "main", branch: "" });
-    console.log("get branch: ", data.tree);
+    } = await octokit.rest.git.getTree({ owner: "xrdavies", repo: "github-db", tree_sha: "db", recursive: 1 });
+    console.log("get tree: ", data.tree);
 
-    data.tree.filter(item => { return item.path.match(/json$/)});
+    let fileArray = data.tree.filter(item => { return item.path.match(/json$/) });
+    console.log("json file list:", fileArray);
+
+    let q = queue(1);
+    fileArray.forEach(item => {
+        q.defer( async (cb) => {
+            const { data } = await octokit.rest.repos.getContent({
+                mediaType: {
+                    format: "raw",
+                },
+                owner: "xrdavies",
+                repo: "github-db",
+                path: item.path,
+                ref: "db"
+            });
+            return cb(null, data);
+        })
+    })
+
+    q.awaitAll((err, res) => {
+        console.log('err:', err);
+        console.log('res:', JSON.parse(res));
+        ;
+    })
 }
+
+
+function get(id, callback) {
+    _getSHA(id, function (err, sha) {
+        if (err) return callback(err);
+        repo.git.blobs(sha).fetch(function (err, res) {
+            if (err) return callback(err);
+            callback(err, JSON.parse(atob(res.content)));
+        });
+    });
+}
+
 
 // export function gitdb(options) {
 //     var oct = Octokit.
